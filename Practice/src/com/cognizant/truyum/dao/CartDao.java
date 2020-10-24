0f8f4bc9f260705ca05f98280dd04bbe953aa6c1
Priyanka-0806 <priyanka0806tweety@gmail.com>
2020-10-24 10:41:41 +0530

    changes made

--- Practice/src/com/cognizant/truyum/dao/CartDao.java
+++ Practice/src/com/cognizant/truyum/dao/CartDao.java
@@ -8,3 +8,29 @@ public class CartDao {
 	}
 
 }
+package com.cognizant.truyum.dao; 
+2 
+ 
+3 import java.util.List; 
+4 
+ 
+5 import com.cognizant.truyum.model.MenuItem; 
+6 
+ 
+7 public interface CartDao { 
+8 	public static void addCartItem(long userId) 
+9 	{ 
+10 		 
+11 	} 
+12 	public static List<MenuItem> getAllCartItems(long userId) throws CartEmptyException 
+13 	{ 
+14 		return null; 
+15 		 
+16 	} 
+17 	public static void removeCartItem(long userUd,long menuItemId) 
+18 	{ 
+19 		 
+20 	} 
+21 	 
+22 	 
+23 } 
---
+++ truYum/src/com/cognizant/truyum/dao/CartDaoCollectionImpl.java
@@ -0,0 +1,32 @@
+package com.cognizant.truyum.dao;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class CartDaoCollectionImpl {
+private static Map<Long,Cart> userCarts =new HashMap<Long,Cart>();
+	
+	public CartDaoCollectionImpl()
+	{
+		
+	}
+	public void addCartItem(long userId,long menuItemId)
+	{
+		
+	}
+	public List<MenuItem> getAllCartItems(long userId) throws CartEmptyException
+	{
+		return null;
+		
+	}
+	public void removeCartItem(long userId,long menuItemId)
+	{
+		
+	}
+
+}
+
+}
